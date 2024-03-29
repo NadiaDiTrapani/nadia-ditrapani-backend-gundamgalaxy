55f9a00a2670d5111c3344237e32dca09f71d327
Nadia Di Trapani <nditrapani@gmail.com>
2024-03-29 11:19:30 -0400

    adjusted owned-controller to be able to filter through booleans

--- controllers/owned-controller.js
+++ controllers/owned-controller.js
@@ -1,20 +1,28 @@
 const knex = require('knex')(require('../knexfile'));
 
-const index = async (req, res) =>{
-    try{
-        const userOwned = await knex('owned').where({user_id: req.params.id});
+const index = async (req, res) => {
+    try {
+        // Fetch user-owned gundams
+        const userOwned = await knex('owned').where({ user_id: req.params.id });
 
-        const gundamId = userOwned.map((ownedItem)=>{
-            return ownedItem.gundam_id
-        })
-        const gundamDets = await knex ('gundams').whereIn('id', gundamId)
+        // Extract gundam IDs from user-owned gundams
+        const gundamIds = userOwned.map((ownedItem) => ownedItem.gundam_id);
 
-        res.status(200).json(gundamDets);
-    } catch(err) {
-        res.status(400).send(`Can't get wishlist:${err}`)
+        // Fetch gundam details for the extracted IDs
+        const gundamDetails = await knex('gundams').whereIn('id', gundamIds);
+
+        // Combine user-owned gundams with their details
+        const userOwnedGundams = userOwned.map((ownedItem) => {
+            const gundamDetail = gundamDetails.find((gundam) => gundam.id === ownedItem.gundam_id);
+            return { ...ownedItem, ...gundamDetail };
+        });
+
+        // Send the combined data in the response
+        res.status(200).json(userOwnedGundams);
+    } catch (err) {
+        res.status(400).send(`Can't get user-owned gundams: ${err}`);
     }
-}
-
+};
 module.exports = {
     index,
-}
+};
